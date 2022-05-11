e8f378769597e29832af6fc5a2d8b25aad0b5045
Suyash <suyash.kathuria7@gmail.com>
2022-05-11 13:19:04 +1200

    Test: Simulate the monty hall game once with the stay choice and get the winning percentage as a number | PASS

--- MontyHallKata/SimulationGenerator.cs
+++ MontyHallKata/SimulationGenerator.cs
@@ -1,8 +1,44 @@
 
+using System;
+
 namespace MontyHallKata
 {
     public class SimulationGenerator
     {
-        
+        private readonly MontyHallGame _game;
+        public SimulationGenerator(IShuffle shuffler)
+        {
+            _game = new MontyHallGame(shuffler);
+        }
+
+        public int Simulate(int numberOfSimulations, string choice)
+        {
+            var gamesWon = 0;
+            for (var i = 0; i < numberOfSimulations; i++)
+            {
+                _game.SetSelectedDoor(1);
+                _game.OpenAnUnselectedLosingDoor();
+
+                bool hasWonGame;
+                if (choice.ToLower().Equals("stay"))
+                {
+                    hasWonGame = _game.HasWonGame();
+                }
+                else
+                {
+                    _game.SwitchDoorSelection();
+                    hasWonGame = _game.HasWonGame();
+                }
+
+                if (hasWonGame)
+                {
+                    gamesWon++;
+                }
+            }
+
+            var winningPercentage = gamesWon * 100 / numberOfSimulations;
+
+            return winningPercentage;
+        }
     }
 }
