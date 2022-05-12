79545d7e6a794071910196599c037647081c01d3
Suyash <suyash.kathuria7@gmail.com>
2022-05-12 17:12:21 +1200

    Adding a random number generator to the randomizer interface

--- MontyHallKata/Models/Randomizer/IRandomizer.cs
+++ MontyHallKata/Models/Randomizer/IRandomizer.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace MontyHallKata.Models.Randomizer
 {
     public interface IRandomizer
     {
         public T[] GetRandomizedArray<T>(T[] array);
+
+        public int GetRandomNumber(int min = 0, int max = int.MaxValue);
     }
 }
