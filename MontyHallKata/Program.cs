e324b158226ad4aff09a83d4a898720c3ee81552
Suyash <suyash.kathuria7@gmail.com>
2022-05-18 16:00:29 +1200

    Implementing the command/query separation into Program.cs

--- MontyHallKata/Program.cs
+++ MontyHallKata/Program.cs
@@ -5,8 +5,11 @@ using MontyHallKata.Models.Randomizer;
 
 
 var simulation = new SimulationGenerator(new CustomRandomizer());
-var stayWinningPercentage = simulation.Simulate(1000, "stay");
-var switchWinningPercentage = simulation.Simulate(1000, "switch");
+simulation.Simulate(1000, "stay");
+var stayWinningPercentage = simulation.GetWinningPercentage();
+simulation.Simulate(1000, "switch");
+var switchWinningPercentage = simulation.GetWinningPercentage();
+
 
 Console.WriteLine(("After a 1000 simulations of each strategy, here are the results:"));
 Console.WriteLine($"Stay Winning Percentage: {stayWinningPercentage}%");
