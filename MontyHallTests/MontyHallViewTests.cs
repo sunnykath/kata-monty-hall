fe1513af68765646563ce902153f548c146cd1a3
Suyash <suyash.kathuria7@gmail.com>
2022-05-23 15:06:31 +1200

    Test: To check if an unselected door opens once a user has selected a door | FAIL

--- MontyHallTests/MontyHallViewTests.cs
+++ MontyHallTests/MontyHallViewTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.IO;
+using MontyHallKata.Models;
 using MontyHallKata.Models.Randomizer;
 using MontyHallKata.Views;
+using Moq;
 using Xunit;
 
 namespace MontyHallTests
@@ -55,10 +57,10 @@ namespace MontyHallTests
         }
 
         [Fact]
-        public void GivenTheDoorsArePrinted_WhenTheUserIsPromptedToSelectADoor_ThenTheUsersSelectionShouldBeDisplayedINThePOutput()
+        public void GivenTheDoorsArePrinted_WhenTheUserIsPromptedToSelectADoor_ThenTheUsersSelectionShouldBeDisplayedInTheOutput()
         {
             // Arrange
-            var stringReader = new StringReader("1\n");
+            var stringReader = new StringReader("1\nq\n");
             var stringWriter = new StringWriter();
             Console.SetIn(stringReader);
             Console.SetOut(stringWriter);
@@ -67,6 +69,31 @@ namespace MontyHallTests
             
             // Act
             _montyHallView.Play(_randomizer);
+            
+            // Assert
+            Assert.Contains(expectedOutcome, stringWriter.ToString());
+        }
+
+        [Fact]
+        public void GivenTheDoorsArePrinted_WhenTheUserHasSelectedADoor_ThenOneOfTheRemainingDoorsShouldOpenAndShouldBeDisplayedInTheOutput()
+        {
+            // Arrange
+            var stringReader = new StringReader("1\nq\n");
+            var stringWriter = new StringWriter();
+            Console.SetIn(stringReader);
+            Console.SetOut(stringWriter);
+            
+            var mockRandomizer = new Mock<IRandomizer>();
+            mockRandomizer.Setup(randomizer => randomizer.GetRandomizedArray(It.IsAny<Door[]>()))
+                .Returns(() => new[]
+                {
+                    DoorsFactory.CreateLosingDoor(), DoorsFactory.CreateWinningDoor(), DoorsFactory.CreateLosingDoor()
+                });
+
+            const string expectedOutcome = "#Door 3#\t#Open#"; 
+            
+            // Act
+            _montyHallView.Play(mockRandomizer.Object);
             
             // Assert
             Assert.Contains(expectedOutcome, stringWriter.ToString());
