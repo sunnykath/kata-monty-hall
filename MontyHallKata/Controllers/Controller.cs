e109ac33e89f63576a51d1b9484080fe88d60303
Suyash <suyash.kathuria7@gmail.com>
2022-06-01 21:24:06 +1200

    Test: Quit option is available when the game has started | PASS

---
+++ MontyHallKata/Controllers/Controller.cs
@@ -0,0 +1,30 @@
+using System.Reflection.Metadata;
+using Microsoft.VisualBasic;
+using MontyHallKata.Models.Randomizer;
+using MontyHallKata.Views;
+
+namespace MontyHallKata.Controllers
+{
+    public class Controller
+    {
+        private MontyHallGame? _game;
+        private MontyHallView _view;
+
+        public Controller()
+        {
+            _view = new MontyHallView(new CustomConsole());;
+        }
+
+        public void Play(CustomRandomizer randomizer)
+        {
+            _game = new MontyHallGame(randomizer);
+
+            var playerInput = new CustomConsole().GetIntInput();
+
+            if (playerInput == 0)
+            {
+                _view.OutputQuitMessage();
+            }
+        }
+    }
+}
