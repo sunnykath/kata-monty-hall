4d354e5e61b76be1c2965366b1f77d0d6d65b47a
Suyash <suyash.kathuria7@gmail.com>
2022-06-01 21:50:56 +1200

    Test: Unselected door is opened and user can switch or stay | FAIL

--- MontyHallTests/MontyHallControllerTests.cs
+++ MontyHallTests/MontyHallControllerTests.cs
@@ -1,7 +1,9 @@
 using System;
 using System.IO;
 using MontyHallKata.Controllers;
+using MontyHallKata.Models;
 using MontyHallKata.Models.Randomizer;
+using Moq;
 using Xunit;
 
 namespace MontyHallTests
@@ -72,61 +74,61 @@ namespace MontyHallTests
             // Assert
             Assert.Contains(expectedOutcome, stringWriter.ToString());
         }
-        //
-        // [Fact]
-        // public void GivenTheDoorsArePrinted_WhenTheUserHasSelectedADoor_ThenOneOfTheRemainingDoorsShouldOpenAndShouldBeDisplayedInTheOutput()
-        // {
-        //     // Arrange
-        //     var stringReader = new StringReader("1\n0\n");
-        //     var stringWriter = new StringWriter();
-        //     Console.SetIn(stringReader);
-        //     Console.SetOut(stringWriter);
-        //     
-        //     var mockRandomizer = new Mock<IRandomizer>();
-        //     mockRandomizer.Setup(randomizer => randomizer.GetRandomizedArray(It.IsAny<Door[]>()))
-        //         .Returns(() => new[]
-        //         {
-        //             DoorsFactory.CreateLosingDoor(), DoorsFactory.CreateWinningDoor(), DoorsFactory.CreateLosingDoor()
-        //         });
-        //
-        //     const string expectedOutcome = "#Door 3#\t#Open#"; 
-        //     
-        //     // Act
-        //     _montyHallView.Play(mockRandomizer.Object);
-        //     
-        //     // Assert
-        //     Assert.Contains(expectedOutcome, stringWriter.ToString());
-        // }
-        //
-        // [Fact]
-        // public void GivenTheUserHasSelectedADoor_WhenOneOfTheRemainingDoorsOpens_ThenTheUserShouldBeAbleToSwitchOrStayWithTheirInitialSelection()
-        // {
-        //     // Arrange
-        //     var stringReader = new StringReader("1\n2\n");
-        //     var stringWriter = new StringWriter();
-        //     Console.SetIn(stringReader);
-        //     Console.SetOut(stringWriter);
-        //     
-        //     var mockRandomizer = new Mock<IRandomizer>();
-        //     mockRandomizer.Setup(randomizer => randomizer.GetRandomizedArray(It.IsAny<Door[]>()))
-        //         .Returns(() => new[]
-        //         {
-        //             DoorsFactory.CreateLosingDoor(), DoorsFactory.CreateWinningDoor(), DoorsFactory.CreateLosingDoor()
-        //         });
-        //
-        //     const string expectedPromptOutput = "Would you like to switch or stay with you selection?:";
-        //     const string expectedDoorsOutput = "#Door 1#\t#Closed#\n" +
-        //                                        "#Door 2#\t#Selected#\n" +
-        //                                        "#Door 3#\t#Open#\n"; 
-        //     
-        //     // Act
-        //     _montyHallView.Play(mockRandomizer.Object);
-        //     
-        //     // Assert
-        //     Assert.Contains(expectedPromptOutput, stringWriter.ToString());
-        //     Assert.Contains(expectedDoorsOutput, stringWriter.ToString());
-        // }
-        //
+        
+        [Fact]
+        public void GivenTheDoorsArePrinted_WhenTheUserHasSelectedADoor_ThenOneOfTheRemainingDoorsShouldOpenAndShouldBeDisplayedInTheOutput()
+        {
+            // Arrange
+            var stringReader = new StringReader("1\n0\n");
+            var stringWriter = new StringWriter();
+            Console.SetIn(stringReader);
+            Console.SetOut(stringWriter);
+            
+            var mockRandomizer = new Mock<IRandomizer>();
+            mockRandomizer.Setup(randomizer => randomizer.GetRandomizedArray(It.IsAny<Door[]>()))
+                .Returns(() => new[]
+                {
+                    DoorsFactory.CreateLosingDoor(), DoorsFactory.CreateWinningDoor(), DoorsFactory.CreateLosingDoor()
+                });
+        
+            const string expectedOutcome = "#Door 3#\t#Open#"; 
+            
+            // Act
+            _controller.Play(mockRandomizer.Object);
+            
+            // Assert
+            Assert.Contains(expectedOutcome, stringWriter.ToString());
+        }
+        
+        [Fact]
+        public void GivenTheUserHasSelectedADoor_WhenOneOfTheRemainingDoorsOpens_ThenTheUserShouldBeAbleToSwitchOrStayWithTheirInitialSelection()
+        {
+            // Arrange
+            var stringReader = new StringReader("1\n2\n0\n");
+            var stringWriter = new StringWriter();
+            Console.SetIn(stringReader);
+            Console.SetOut(stringWriter);
+            
+            var mockRandomizer = new Mock<IRandomizer>();
+            mockRandomizer.Setup(randomizer => randomizer.GetRandomizedArray(It.IsAny<Door[]>()))
+                .Returns(() => new[]
+                {
+                    DoorsFactory.CreateLosingDoor(), DoorsFactory.CreateWinningDoor(), DoorsFactory.CreateLosingDoor()
+                });
+        
+            const string expectedPromptOutput = "Would you like to switch or stay with you selection?:";
+            const string expectedDoorsOutput = "#Door 1#\t#Closed#\n" +
+                                               "#Door 2#\t#Selected#\n" +
+                                               "#Door 3#\t#Open#\n"; 
+            
+            // Act
+            _controller.Play(mockRandomizer.Object);
+            
+            // Assert
+            Assert.Contains(expectedPromptOutput, stringWriter.ToString());
+            Assert.Contains(expectedDoorsOutput, stringWriter.ToString());
+        }
+        
         // [Fact]
         // public void GivenOneOfTheRemainingDoorsHasOpened_WhenTheUserHasMadeTheirChoice_ThenTheResultOfTheGameShouldBeRevealed()
         // {
